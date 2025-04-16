0c617dabca1a83fca9143c14fbb830426ddcaad2
EmeritusThinkPad <jairparedesal123@gmail.com>
2025-04-15 21:09:34 -0500

    config: inicializa el proyecto

---
+++ src/test/java/org/alquimiasoft/minegocio/MinegocioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alquimiasoft.minegocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinegocioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
